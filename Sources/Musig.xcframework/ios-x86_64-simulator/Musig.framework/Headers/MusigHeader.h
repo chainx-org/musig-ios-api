0834490aeaf9f53ec77a3a9efb196d72b58daac1
AAweidai <1253464613@qq.com>
2021-11-09 18:20:38 +0800

    Add message param

--- Sources/Musig.xcframework/ios-x86_64-simulator/Musig.framework/Headers/MusigHeader.h
+++ Sources/Musig.xcframework/ios-x86_64-simulator/Musig.framework/Headers/MusigHeader.h
@@ -18,7 +18,7 @@ const char *get_my_pubkey(const char *privkey);
 
 typedef struct Musig Musig;
 
-Musig *get_musig(const char *privkey);
+Musig *get_musig(uint32_t message, const char *privkey);
 
 char *encode_reveal_stage(Musig *musig);
 
@@ -30,7 +30,7 @@ Musig *cosign_stage(Musig *musig, const char *reveals,const char *pubkeys);
 
 char *get_my_cosign(Musig *musig);
 
-char *get_signature(const char *reveals, const char *pubkeys, const char *cosign);
+char *get_signature(uint32_t message, const char *reveals, const char *pubkeys, const char *cosign);
 
 char *get_agg_pubkey(const char *pubkeys);
 
